2bd999df5d91bf0ab5d96768c030d3d1b4667f2f
Imants Kromāns <981273@inbox.lv>
2022-09-13 22:07:42 +0300

    secondBranch

---
+++ secondhw/src/main/java/io/codelex/loops/practice/Exercise5.java
@@ -0,0 +1,27 @@
+package io.codelex.loops.practice;
+
+import java.util.Scanner;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+
+        Scanner text = new Scanner(System.in);
+
+        do {
+            System.out.println("Enter first word: ");
+            String word1 = text.nextLine();
+            System.out.println("Enter second word: ");
+            String word2 = text.nextLine();
+
+            int dotLength = 30 - word1.length() - word2.length();
+
+            System.out.print(word1);
+            for (int i = 0; i < dotLength; i++) {
+                System.out.print(".");
+            }
+            System.out.println(word2 + "\n");
+
+        }
+        while (true);
+    }
+}
