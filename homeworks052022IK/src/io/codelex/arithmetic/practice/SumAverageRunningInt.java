473fb07cb5a304d319d59ef9438bd9dae0f22d46
Imants Kromāns <981273@inbox.lv>
2022-09-16 23:12:14 +0300

    correction for the first homework

--- homeworks052022IK/src/io/codelex/arithmetic/practice/SumAverageRunningInt.java
+++ homeworks052022IK/src/io/codelex/arithmetic/practice/SumAverageRunningInt.java
@@ -2,7 +2,7 @@ package io.codelex.arithmetic.practice;
 
 public class SumAverageRunningInt {
     public static void main(String[] args) {
-        int lowerBound = 1;
+        int lowerBound = 30;
         int upperBound = 100;
         int sum = 0;
 
@@ -10,7 +10,7 @@ public class SumAverageRunningInt {
             sum += number;
         }
 
-        double average = (double) sum / upperBound;
+        double average = (double) sum / (upperBound - lowerBound + 1) ;
         System.out.println("The sum of " + lowerBound + " to " + upperBound + " is " + sum);
         System.out.println("The average is " + average);
     }
