4f804c4779e0f8dd7420eaca79ed32a3443820f9
Imants Kromāns <981273@inbox.lv>
2022-09-21 20:53:21 +0300

    main test java

--- thirdHomeWork/src/Main.java
+++ thirdHomeWork/src/Main.java
@@ -1,7 +1,10 @@
-import java.util.Arrays;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Objects;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
     }
 }
