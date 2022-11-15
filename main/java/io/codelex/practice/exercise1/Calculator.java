612b2ced1b5ca7ee242432beafea6b433140e44d
Imants Kromāns <981273@inbox.lv>
2022-11-15 13:34:33 +0200

    correction of mistakes and rearranging folder structure

---
+++ main/java/io/codelex/practice/exercise1/Calculator.java
@@ -0,0 +1,36 @@
+package io.codelex.practice.exercise1;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Calculator {
+    private final String startDate;
+    private final String endDate;
+    private final int workHours = 8;
+
+    public Calculator(String startDate, String endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public void calculateWorkingDays() {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            Calendar start = Calendar.getInstance();
+            start.setTime(sdf.parse(startDate));
+            Calendar end = Calendar.getInstance();
+            end.setTime(sdf.parse(endDate));
+            int workingDays = 0;
+            while (!start.after(end)) {
+                int day = start.get(Calendar.DAY_OF_WEEK);
+                if ((day != Calendar.SATURDAY) && (day != Calendar.SUNDAY))
+                    workingDays++;
+                start.add(Calendar.DATE, 1);
+            }
+            System.out.println("There is " + workingDays * workHours + " work hours in this period.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
